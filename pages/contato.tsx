1490f426ee5ee5150b761799acedcf87437a6cb2
ridibrito <devoldbsb@gmail.com>
2022-10-23 19:56:21 -0300

    initial2

--- pages/contato.tsx
+++ pages/contato.tsx
@@ -22,7 +22,7 @@ export default function Servicos() {
          
           <div className="sm:col-span-2">
               <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-300 dark:text-gray-400">Your message</label>
-              <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900    bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Deixe sua mensagem"></textarea>
+              <textarea id="message"  className="block p-2.5 w-full text-sm text-gray-900    bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Deixe sua mensagem"></textarea>
           </div>
           <button type="submit" className="py-3 px-8 text-sm font-semibold text-center text-white rounded bg-primary-blue  hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Enviar</button>
       </form>
